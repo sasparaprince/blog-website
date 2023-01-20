adbccf2b1292dda9bf34b74c60634f836c74dea2
prince saspara <106681231+sasparaprince@users.noreply.github.com>
2023-01-20 12:41:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-prince:Prince3252@cluster0.xkujutz.mongodb.net/Blog", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
@@ -75,6 +75,11 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
